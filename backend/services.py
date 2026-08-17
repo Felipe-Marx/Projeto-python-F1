11697ac4c15fbaa5fade77f92e509cda9afe7854
felipemarx-svg <felipemarxvs@gmail.com>
2026-08-17 00:01:12 -0300

    feat: otimiza a função de ranking de pilotos utilizando list comprehension

--- backend/services.py
+++ backend/services.py
@@ -74,11 +74,7 @@ def delete_driver(driver_id: int):
 def get_driver_ranking(limit:int):
     drivers = get_all_drivers()
     ranking = sorted(drivers, key=lambda driver: driver["points"], reverse=True)
-    ranking_with_position = []
-    for index, driver in enumerate(ranking, start=1):
-        driver_with_position = {
-    **driver,
-    "position": index
-}
-        ranking_with_position.append(driver_with_position)
+    ranking_with_position = [
+        {**driver, "position": index} for index, driver in enumerate(ranking, start=1)
+    ]
     return ranking_with_position[:limit]
